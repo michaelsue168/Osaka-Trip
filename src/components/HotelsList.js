42a3736d3734a83c1cec411e34741a1308df3737
Yu-Hsuan <p10400857@pic.net.tw>
2024-05-17 23:05:31 +0800

    add 2

--- src/components/HotelsList.js
+++ src/components/HotelsList.js
@@ -41,14 +41,53 @@ export const hotels = [
       { name: 'M 御堂筋線', color: 'red' },
       { name: '近心齋橋', color: 'blue' },
       { name: '雙人房', color: 'gray' },
-      { name: '12平方公尺', color: 'gray' }
+      { name: '12平方公尺', color: 'gray' },
+      { name: '大阪', color: 'gray' }
     ]
   },
   {
     id: 2,
     img: [
-      { id: 1, src: 'https://via.placeholder.com/1080x400' },
-      { id: 2, src: 'https://via.placeholder.com/1080x400' }
+      {
+        id: 1,
+        src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/473037427.jpg?k=4b6dcebc4b35ea5b6926b10638642040619e26fd76657c2c1c2675e4e7dbfc4a&o=&hp=1'
+      },
+      {
+        id: 2,
+        src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/473037441.jpg?k=8000161b487af32bb5fccf675e3e26608b4f34bb5342850c4336c0ada42f8ad9&o=&hp=1'
+      },
+      {
+        id: 3,
+        src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/473037447.jpg?k=ece18a0c158330053c0641d9f0867afd256efc17fad59eb96d75d95a07ba5931&o=&hp=1'
+      },
+      {
+        id: 4,
+        src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/473037437.jpg?k=7b62ca8b6a5975de379f9db2478ceba5a0902638992045dbd7161e8f6a043b3c&o=&hp=1'
+      },
+      {
+        id: 5,
+        src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/473037448.jpg?k=7f914c6702be6af36db50e9371555f7b40c5acdba2dd1dda81e846091dde7f5f&o=&hp=1'
+      },
+      {
+        id: 6,
+        src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/473037433.jpg?k=8319fa89e7c349afe8b638cc60b1f5a93a8f7ec66289bdfa5214cc06077763f8&o=&hp=1'
+      },
+      {
+        id: 7,
+        src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/473037436.jpg?k=75be509f4a2f3601c45fd05b80a1dfd20da9b555ae694c628b5cb65f5fa4921b&o=&hp=1'
+      },
+      {
+        id: 8,
+        src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/473042771.jpg?k=e5b79e7253529a23f48d3ddd2bb7f5c80462d726bd68dc97cc0a32429b830131&o=&hp=1'
+      },
+      {
+        id: 9,
+        src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/473037450.jpg?k=60571d5fccba248e627e993a1797abc298977bf5d04a1b927e2696b2da4b0eac&o=&hp=1'
+      },
+      {
+        id: 10,
+        src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/473037449.jpg?k=aa606dacf3e969742e0c1e333c28fff04039ecde75fb02adb91150c2a8e32b13&o=&hp=1'
+      }
     ],
     name: 'Grandia Shinsaibashi ',
     info: '35平方公尺 | ',
@@ -60,18 +99,34 @@ export const hotels = [
       { name: 'M 御堂筋線', color: 'red' },
       { name: '近心齋橋', color: 'blue' },
       { name: '單臥室公寓', color: 'gray' },
-      { name: '25平方公尺', color: 'gray' }
+      { name: '25平方公尺', color: 'gray' },
+      { name: '大阪', color: 'gray' }
     ]
-  },
-  {
-    id: 3,
-    img: [
-      { id: 1, src: 'https://via.placeholder.com/1080x400' },
-      { id: 2, src: 'https://via.placeholder.com/1080x400' }
-    ],
-    name: 'Lorem3',
-    info: '18平方公尺 | ',
-    good: ['1', '1', '1', '1', '1'],
-    bad: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, dolor?'
   }
+  // {
+  //   id: 3,
+  //   img: [
+  //     {
+  //       id: 1,
+  //       src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/473037427.jpg?k=4b6dcebc4b35ea5b6926b10638642040619e26fd76657c2c1c2675e4e7dbfc4a&o=&hp=1'
+  //     },
+  //     {
+  //       id: 2,
+  //       src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/473037441.jpg?k=8000161b487af32bb5fccf675e3e26608b4f34bb5342850c4336c0ada42f8ad9&o=&hp=1'
+  //     },
+  //   ],
+  //   name: 'Lorem3',
+  //   info: '18平方公尺 | ',
+  //   good: ['1', '1', '1', '1', '1'],
+  //   bad: ['1', '1', '1', '1', '1'],
+  //   link: 'https://maps.app.goo.gl/iNXM4p3QTWYr1W7a8',
+  //   tag: [
+  //     { name: '$ 3,758', color: 'gold' },
+  //     { name: 'M 御堂筋線', color: 'red' },
+  //     { name: '近心齋橋', color: 'blue' },
+  //     { name: '單臥室公寓', color: 'gray' },
+  //     { name: '25平方公尺', color: 'gray' },
+  //     { name: '大阪', color: 'gray' },
+  //   ]
+  // }
 ]
